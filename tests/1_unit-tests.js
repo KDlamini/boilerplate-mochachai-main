ca9a6a9d04b2553cb119563c15cb5c9f4c367a01
Simo Dlamini <wilzmar.kwd@gmail.com>
2021-03-22 11:15:00 +0000

    test deepEqual and notDeepEqual

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -43,12 +43,12 @@ suite('Unit Tests', function () {
       assert.notStrictEqual(6, '6');
       assert.strictEqual(6, 3 * 2);
       assert.strictEqual(6 * '2', 12);
-      assert.notStrictEquall([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
     });
   });
 
